b413efe9a722dd2eb6996cca3ae2ee0f43880170
Wallace Martins <wallacemartinss@gmail.com>
2026-04-01 10:53:14 -0300

    Switch download URLs from GitHub to Cloudflare R2

--- Casks/devhub.rb
+++ Casks/devhub.rb
@@ -2,12 +2,12 @@ cask "devhub" do
   version "1.9.3"
 
   on_arm do
-    url "https://github.com/hubdev-io/devhub-go/releases/download/v#{version}/DevHub-arm64.dmg"
+    url "https://pub-51b22bf3fd5a4d73a4ed580105cfc09f.r2.dev/v#{version}/DevHub-macos-arm64.dmg"
     sha256 "24c68d7a7cedd4d761e1ff9f586fa4cef909e2bcc1db2fab7041b5fe08906080"
   end
 
   on_intel do
-    url "https://github.com/hubdev-io/devhub-go/releases/download/v#{version}/DevHub-amd64.dmg"
+    url "https://pub-51b22bf3fd5a4d73a4ed580105cfc09f.r2.dev/v#{version}/DevHub-macos-amd64.dmg"
     sha256 "fc65af867084a6a0f6ef861cc846159ea01e990826f5a81c3f3ec4b132a7ecaf"
   end
 
